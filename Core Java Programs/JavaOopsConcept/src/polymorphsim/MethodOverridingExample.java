bab0d6baf5d2ecadda15a27a8418dd18c83681a7
akash <akash300383@gmail.com>
2024-04-24 13:50:15 +0530

    done

---
+++ Core Java Programs/JavaOopsConcept/src/polymorphsim/MethodOverridingExample.java
@@ -0,0 +1,31 @@
+package polymorphsim;
+class Bike {
+	void speed() {
+		System.out.println("60km/hr");
+	}
+}
+class Honda extends Bike {
+	void color() {
+		System.out.println("Black");
+	}
+}
+class Pulsar extends Bike {
+	//@Override
+	void color() {
+		System.out.println("Gray");
+	}
+	@Override
+	void speed() {
+		System.out.println("90km/hr");
+	}
+}
+
+public class MethodOverridingExample {
+	public static void main(String[] args) {
+		Honda hh = new Honda();
+		hh.color();  hh.speed();
+		Pulsar pu = new Pulsar();
+		pu.color();  pu.speed();
+	}
+
+}
