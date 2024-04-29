88900418cc104f27df42ebc34a4c8032b668ec96
akash <akash300383@gmail.com>
2024-04-29 14:15:00 +0530

    done

--- Core Java Programs/ExceptionHndlingExamples/src/com/RuntimeExceptionExample.java
+++ Core Java Programs/ExceptionHndlingExamples/src/com/RuntimeExceptionExample.java
@@ -5,14 +5,20 @@ public class RuntimeExceptionExample {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		int a=10;
-		int b=0;
+		int b=1;
+		int abc[]= {10,20,30,40};
+		String xyz="a10";
 			try {
 			int res = a/b;
 			System.out.println("Result is "+res);
+			int res1 = abc[0]/100;
+			System.out.println("Result is "+res1);
+			System.out.println(xyz+10);
+			System.out.println(Integer.parseInt(xyz)+10);
 			}catch(Exception e) {
 				//System.out.println("I Take Care!");
 				//System.out.println(e.getMessage()); // short exception message 
-				//System.out.println(e.toString());
+				System.out.println(e.toString());
 			}
 		System.out.println("Bye...");
 		System.out.println("Bye...");
