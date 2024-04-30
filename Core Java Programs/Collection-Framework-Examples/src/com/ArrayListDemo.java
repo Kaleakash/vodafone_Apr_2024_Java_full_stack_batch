d9f2d9a91439152cd47221b5a92235a04874a3c9
akash <akash300383@gmail.com>
2024-04-30 16:52:25 +0530

    done

--- Core Java Programs/Collection-Framework-Examples/src/com/ArrayListDemo.java
+++ Core Java Programs/Collection-Framework-Examples/src/com/ArrayListDemo.java
@@ -15,7 +15,7 @@ public class ArrayListDemo {
 		al.add(6);
 		System.out.println(al);
 		System.out.println("Get value using index position "+al.get(1));
-		al.add(1, 50);		// 1st parameter index position, 2nd parameter value 
+			al.add(1, 50);		// 1st parameter index position, 2nd parameter value 
 		System.out.println(al);
 		al.set(3, 200);		// 3 index position value replace by 200
 		System.out.println(al);
@@ -25,6 +25,8 @@ public class ArrayListDemo {
 		al.remove(abc);  // remove using value
 		System.out.println("Search "+al.contains(6));
 		System.out.println("Search "+al.contains(60));
+		System.out.println("First elements "+al.get(0));
+		System.out.println("last elements "+al.get(al.size()-1));
 	}
 
 }
---
+++ Core Java Programs/Collection-Framework-Examples/src/com/LinkedListDemo.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.add(10);
+		ll.add(20);
+					// 100 get added... 
+		ll.add(30);
+		ll.add(40);
+		ll.add(50);
+		System.out.println(ll);
+		ll.add(2,100);
+		ll.remove(4);
+		System.out.println("get first elements "+ll.getFirst());
+		System.out.println("Get last elements "+ll.getLast());
+	}
+
+}
