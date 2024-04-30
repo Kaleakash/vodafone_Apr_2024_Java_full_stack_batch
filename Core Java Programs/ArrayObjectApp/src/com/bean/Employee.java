621be8d7a2d319ea528d64400691ac41fd928458
akash <akash300383@gmail.com>
2024-04-30 14:20:43 +0530

    done

--- Core Java Programs/ArrayObjectApp/src/com/bean/Employee.java
+++ Core Java Programs/ArrayObjectApp/src/com/bean/Employee.java
@@ -22,5 +22,25 @@ public float getSalary() {
 public void setSalary(float salary) {
 	this.salary = salary;
 }
+public Employee() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Employee(int id, String name, float salary) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.salary = salary;
+}
+//@Override
+//public String toString() {
+//	return "Employee class object";
+//}
+@Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+}
+
+
 
 }
--- Core Java Programs/ArrayObjectApp/src/com/main/DemoTest.java
+++ Core Java Programs/ArrayObjectApp/src/com/main/DemoTest.java
@@ -10,9 +10,17 @@ public class DemoTest {
 		emp1.setName("Ravi");
 		emp1.setSalary(14000);
 		
+		Employee emp2 = new Employee(101, "Steven", 24000);
+		
 		System.out.println("id is "+emp1.getId());
 		System.out.println("name is "+emp1.getName());
 		System.out.println("salary is "+emp1.getSalary());
+		
+		System.out.println("id is "+emp2.getId());
+		System.out.println("name is "+emp2.getName());
+		System.out.println("salary is "+emp2.getSalary());
+		
+		System.out.println(emp1);
 	}
 
 }
