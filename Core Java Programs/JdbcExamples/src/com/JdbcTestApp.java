f7d33e09bc182e9a1e6aae1024489db482463ff7
akash <akash300383@gmail.com>
2024-05-08 17:18:09 +0530

    done

--- Core Java Programs/JdbcExamples/src/com/JdbcTestApp.java
+++ Core Java Programs/JdbcExamples/src/com/JdbcTestApp.java
@@ -5,7 +5,12 @@ public class JdbcTestApp {
 
 	public static void main(String[] args) {
 		try {
-			
+	Class.forName("com.mysql.cj.jdbc.Driver");		// mysql database 
+	//Class.forName("oracle.jdbc.driver.OracleDriver");	// oracle database 
+	System.out.println("Driver loaded succesfully");
+Connection con = 
+DriverManager.getConnection("jdbc:mysql://localhost:3306/my_training_db", "root", "root@123");
+System.out.println("Connected successfully");
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
