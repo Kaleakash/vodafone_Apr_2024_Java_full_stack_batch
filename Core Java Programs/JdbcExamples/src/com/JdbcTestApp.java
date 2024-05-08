48b7d2c0edb5266bf8e59df111871f394ae0da96
akash <akash300383@gmail.com>
2024-05-08 16:57:02 +0530

    done

---
+++ Core Java Programs/JdbcExamples/src/com/JdbcTestApp.java
@@ -0,0 +1,15 @@
+package com;
+import java.sql.*;
+
+public class JdbcTestApp {
+
+	public static void main(String[] args) {
+		try {
+			
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+
+	}
+
+}
