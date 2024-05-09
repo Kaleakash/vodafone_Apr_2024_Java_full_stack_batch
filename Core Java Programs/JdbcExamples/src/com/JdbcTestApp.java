cbb08e22055aaefcb9b07522f2a102e470fb4afa
akash <akash300383@gmail.com>
2024-05-09 14:00:24 +0530

    done

--- Core Java Programs/JdbcExamples/src/com/JdbcTestApp.java
+++ Core Java Programs/JdbcExamples/src/com/JdbcTestApp.java
@@ -10,7 +10,37 @@ public class JdbcTestApp {
 	System.out.println("Driver loaded succesfully");
 Connection con = 
 DriverManager.getConnection("jdbc:mysql://localhost:3306/my_training_db", "root", "root@123");
+//Connection con = 
+//DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger");
+
 System.out.println("Connected successfully");
+Statement stmt = con.createStatement();
+
+//	// insert Query 
+//	int result = stmt.executeUpdate("insert into product values(103,'Pen Drive',2500)");
+//	if(result>0) {
+//		System.out.println("Record inserted successfully");
+//	}
+
+//	//delete Query 
+//	int result = stmt.executeUpdate("delete from product where pid=103");
+//	if(result>0) {
+//		System.out.println("Record deleted successfully");
+//	}else {
+//		System.out.println("Record not present");
+//	}
+
+
+	//update Query 
+	int result = stmt.executeUpdate("update product set price=60000 where pid=100");
+	if(result>0) {
+	System.out.println("Record updated successfully");
+	}else {
+	System.out.println("Record not present");
+	}
+	
+	stmt.close();
+	con.close();
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
