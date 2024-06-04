58b02573ebf8a74e2a429529ced849647e6168c1
akash <akash300383@gmail.com>
2024-06-04 15:13:58 +0530

    done

--- course 3/Spring Framework/DIUsingXML/src/main/java/com/DemoTest.java
+++ course 3/Spring Framework/DIUsingXML/src/main/java/com/DemoTest.java
@@ -1,10 +1,21 @@
 package com;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class DemoTest {
 
 	public static void main(String[] args) {
-		Employee emp = new Employee();
-		emp.display();
+//		Employee emp = new Employee();
+//		emp.display();
+		
+		Resource res  = new ClassPathResource("beans.xml"); // load the xml file 
+		BeanFactory obj = new XmlBeanFactory(res);   // get beanfactory interface reference. 
+		
+		Employee employee1 = (Employee)obj.getBean("emp1"); // pull the object from container 
+		employee1.display();
 	}
 
 }
