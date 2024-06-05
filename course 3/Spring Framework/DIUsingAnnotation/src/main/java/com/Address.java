6757a97386311ab6640ce23a3b208bef7cfc40b7
akash <akash300383@gmail.com>
2024-06-05 14:06:22 +0530

    done

---
+++ course 3/Spring Framework/DIUsingAnnotation/src/main/java/com/Address.java
@@ -0,0 +1,38 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component      			 // <bean class="com.Employee"></bean> by default id is class name using camel naming rule 
+public class Address {		// address,
+@Value(value = "Mumbai")
+private String city;
+@Value(value = "Mh")
+private String state;
+public Address() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Address(String city, String state) {
+	super();
+	this.city = city;
+	this.state = state;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+@Override
+public String toString() {
+	return "Address [city=" + city + ", state=" + state + "]";
+}
+
+}
---
+++ course 3/Spring Framework/DIUsingAnnotation/src/main/java/com/DemoTest.java
@@ -0,0 +1,26 @@
+package com;
+
+//import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		//Resource res  = new ClassPathResource("beans.xml"); // load the xml file 
+		//BeanFactory obj = new XmlBeanFactory(res);   // get beanfactory interface reference. 
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		Address add = (Address)ac.getBean("address"); // id is address by defautl class name
+		System.out.println(add);			// it will call toString method 
+		add.setCity("Bangalore");
+		add.setState("Kar");
+		System.out.println(add);
+		Employee emp = (Employee)ac.getBean("employee");
+		System.out.println(emp);
+	}
+
+}
