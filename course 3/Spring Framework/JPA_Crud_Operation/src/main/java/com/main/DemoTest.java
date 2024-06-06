7753636c22376e4bc77c94aa1eb3cac8cb025ac1
akash <akash300383@gmail.com>
2024-06-06 16:50:48 +0530

    done

--- course 3/Spring Framework/JPA_Crud_Operation/src/main/java/com/main/DemoTest.java
+++ course 3/Spring Framework/JPA_Crud_Operation/src/main/java/com/main/DemoTest.java
@@ -18,15 +18,39 @@ public class DemoTest {
 	EntityManager manager = emf.createEntityManager(); // it is like a Statement or PreparedStatement. 
 	EntityTransaction tran = manager.getTransaction();		// get transaction object. 
 	// insert record 
-	Employee emp1 = new Employee();
-	emp1.setId(100);
-	emp1.setName("Ravi");
-	emp1.setSalary(34000);
+//	Employee emp1 = new Employee();
+//	emp1.setId(102);
+//	emp1.setName("Ajay");
+//	emp1.setSalary(45000);
+//	
+//	tran.begin();
+//		manager.persist(emp1);   // save the record ie insert query 
+//	tran.commit();
+//	System.out.println("Record inserted...");
 	
-	tran.begin();
-		manager.persist(emp1);   // save the record ie insert query 
-	tran.commit();
-	System.out.println("Record inserted...");
+	// Delete Query 
+//	Employee emp1 = manager.find(Employee.class, 100);	// select * from employee where id  = 101;
+//	if(emp1==null) {
+//		System.out.println("No record present");
+//	}else {
+//		tran.begin();
+//		manager.remove(emp1);			// like delete query 
+//		tran.commit();
+//		System.out.println("Record deleted successfully");
+//	}
+	
+	
+	// update Query 
+		Employee emp1 = manager.find(Employee.class, 100);	// select * from employee where id  = 101;
+		if(emp1==null) {
+			System.out.println("No record present");
+		}else {
+			tran.begin();
+			emp1.setSalary(35000);
+			manager.merge(emp1);				// update query 
+			tran.commit();
+			System.out.println("Record updated successfully");
+		}
 	}
 
 }
