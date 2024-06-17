dc85c797cc4d307fcc2ff0e6325d155c456c1cce
akash <akash300383@gmail.com>
2024-06-17 14:45:42 +0530

    done

--- course 3/Spring boot workspace/SpringBootMVCWithJPAWithThymeleaf/SpringBootMVCWithJPAWithThymeleaf/src/main/java/com/SpringBootMvcWithJpaWithThymeleafApplication.java
+++ course 3/Spring boot workspace/SpringBootMVCWithJPAWithThymeleaf/SpringBootMVCWithJPAWithThymeleaf/src/main/java/com/SpringBootMvcWithJpaWithThymeleafApplication.java
@@ -2,8 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")  // component scan for @Controller annotation, @service and @Repository 
+@EntityScan(basePackages = "com.bean")  // Enable @Entity annotation 
+@EnableJpaRepositories(basePackages = "com.dao")  // spring jpa data enable 
 public class SpringBootMvcWithJpaWithThymeleafApplication {
 
 	public static void main(String[] args) {
