0aa5a5032916e5e122ecc5a0093f7920d32ba7c0
akash <akash300383@gmail.com>
2024-06-18 16:04:47 +0530

    done

--- course 3/Spring boot workspace/SpringBootRestAPI/SpringBootRestAPI/src/main/java/com/controller/SimpleController.java
+++ course 3/Spring boot workspace/SpringBootRestAPI/SpringBootRestAPI/src/main/java/com/controller/SimpleController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,4 +15,21 @@ public class SimpleController {
 	public @ResponseBody String sayHell() {
 		return "Welcome to Spring boot rest api";
 	}
+	// http://locahost:9191/plain 
+	@RequestMapping(value = "plain",method = RequestMethod.GET,produces = MediaType.TEXT_PLAIN_VALUE)
+	public @ResponseBody String sayHelloInPlain() {
+		return "<h2>Welcome to Spring boot rest api</h2>";
+	}
+	// http://locahost:9191/plain 
+		@RequestMapping(value = "html",method = RequestMethod.GET,produces = MediaType.TEXT_HTML_VALUE)
+		public @ResponseBody String sayHelloInHtml() {
+			return "<h2>Welcome to Spring boot rest api</h2>";
+		}
+		
+		// http://locahost:9191/plain 
+		@RequestMapping(value = "xml",method = RequestMethod.GET,produces = MediaType.TEXT_XML_VALUE)
+		public @ResponseBody String sayHelloInXml() {
+			return "<h2>Welcome to Spring boot rest api</h2>";
+		}	
+		
 }
