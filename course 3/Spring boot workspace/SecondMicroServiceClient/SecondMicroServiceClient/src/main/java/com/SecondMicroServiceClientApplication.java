dd77b9dacda84edfc6e995389b68b38795bede21
akash <akash300383@gmail.com>
2024-06-24 14:49:48 +0530

    done

--- course 3/Spring boot workspace/SecondMicroServiceClient/SecondMicroServiceClient/src/main/java/com/SecondMicroServiceClientApplication.java
+++ course 3/Spring boot workspace/SecondMicroServiceClient/SecondMicroServiceClient/src/main/java/com/SecondMicroServiceClientApplication.java
@@ -2,12 +2,15 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
 public class SecondMicroServiceClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondMicroServiceClientApplication.class, args);
+		System.err.println("second micro service up!");
 	}
 
 }
