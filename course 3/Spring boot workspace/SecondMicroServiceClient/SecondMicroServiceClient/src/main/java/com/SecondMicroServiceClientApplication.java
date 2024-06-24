4692ca87f3797995e090bed96398c6e32cb70a07
akash <akash300383@gmail.com>
2024-06-24 14:47:09 +0530

    done

---
+++ course 3/Spring boot workspace/SecondMicroServiceClient/SecondMicroServiceClient/src/main/java/com/SecondMicroServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondMicroServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondMicroServiceClientApplication.class, args);
+	}
+
+}
