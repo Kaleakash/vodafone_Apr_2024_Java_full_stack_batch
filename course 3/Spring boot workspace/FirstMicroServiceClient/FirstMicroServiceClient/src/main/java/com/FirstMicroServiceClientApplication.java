e6effdbf47953c4b5f5448deafc57ae9612370d1
akash <akash300383@gmail.com>
2024-06-24 14:30:31 +0530

    done

--- course 3/Spring boot workspace/FirstMicroServiceClient/FirstMicroServiceClient/src/main/java/com/FirstMicroServiceClientApplication.java
+++ course 3/Spring boot workspace/FirstMicroServiceClient/FirstMicroServiceClient/src/main/java/com/FirstMicroServiceClientApplication.java
@@ -2,12 +2,15 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
 public class FirstMicroServiceClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstMicroServiceClientApplication.class, args);
+		System.err.println("first micro service project up!");
 	}
 
 }
