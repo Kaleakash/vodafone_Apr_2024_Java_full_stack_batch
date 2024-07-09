51af9c19046aa236b13dd0abbd10fcc9f90c4a3e
akash <akash300383@gmail.com>
2024-07-09 16:08:56 +0530

    done

--- course 3/Testing/Spring-Boot-With-Testing/Spring-Boot-With-Testing/src/test/java/com/controller/LoginControllerTest.java
+++ course 3/Testing/Spring-Boot-With-Testing/Spring-Boot-With-Testing/src/test/java/com/controller/LoginControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.entity.Login;
+import com.repository.LoginRepository;
 import com.service.LoginService;
 
 @ExtendWith(MockitoExtension.class)		// it help to create mock or fake object. 
@@ -25,6 +26,9 @@ class LoginControllerTest {
 	@InjectMocks
 	LoginController loginController;		// inject that mock in dao layer 
 	
+	@Mock
+	LoginRepository loginRepository;
+	
 	@Test
 	@Disabled
 	void testSignIn() {
@@ -37,7 +41,8 @@ class LoginControllerTest {
 		ll.setEmailid("ravi@gmail.com");
 		ll.setPassword("123");
 		// fake result 
-		Mockito.when(loginService.signUp(ll)).thenReturn("failure");		// fake information ready 
+		//Mockito.when(loginService.signUp(ll)).thenReturn("failure");		// fake information ready 
+		Mockito.when(loginService.signUp(ll)).thenCallRealMethod();		// fake information ready 
 		
 		// testing 
 		String result = loginController.signUp(ll);
