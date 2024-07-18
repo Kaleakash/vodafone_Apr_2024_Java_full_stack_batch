560692fe2f1ed36b3c7074cf70b73e5f0b7dc80e
akash <akash300383@gmail.com>
2024-07-18 14:38:56 +0530

    done

--- capstone project/frontend-app/frontend-app/src/components/AdminDashboard.js
+++ capstone project/frontend-app/frontend-app/src/components/AdminDashboard.js
@@ -1,9 +1,13 @@
+import Logout from "./Logout";
+
 function AdminDashboard() {
 
 
     return(
         <div>
            <h2>Welcome to Admin Dashboard</h2>
+
+           <Logout></Logout>
         </div>
     )
 }
--- capstone project/frontend-app/frontend-app/src/components/CustomerDashboard.js
+++ capstone project/frontend-app/frontend-app/src/components/CustomerDashboard.js
@@ -1,9 +1,13 @@
+import Logout from "./Logout";
+
 function CustomerDashboard() {
 
 
     return(
         <div>
            <h2>Welcome to Customer Dashboard</h2>
+
+           <Logout></Logout>
         </div>
     )
 }
