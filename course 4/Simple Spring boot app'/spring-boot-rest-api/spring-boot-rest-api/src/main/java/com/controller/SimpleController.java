eac8e80170ca9f5f5056f3f5de88cb43f83395a6
akash <akash300383@gmail.com>
2024-07-18 16:53:07 +0530

    done

---
+++ course 4/Simple Spring boot app'/spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/controller/SimpleController.java
@@ -0,0 +1,15 @@
+
+
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+	@GetMapping(value = "/")
+	public String greeting() {
+		return "Welcome to Spring boot created by Akash Kale!";
+	}
+}
